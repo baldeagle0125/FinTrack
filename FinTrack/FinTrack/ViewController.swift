b7e85a5fd5a62a1f19305df72d03fe3adb5cc2e2
Igor <73532651+mr-igor25@users.noreply.github.com>
2021-09-04 15:09:07 +0300

    Added launch screen

--- FinTrack/FinTrack/ViewController.swift
+++ FinTrack/FinTrack/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var amountTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+              
+        //Looks for single or multiple taps.
+         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+
+        view.addGestureRecognizer(tap)
     }
 
-
+    //Calls this function when the tap is recognized.
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    @IBAction func newExpenseButton(_ sender: Any) {
+    }
+    
+    @IBAction func newIncomeButton(_ sender: Any) {
+    }
 }
 
