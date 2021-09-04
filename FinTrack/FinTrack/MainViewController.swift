51e581e0a174a0feca1109b275503688a633a61b
Igor <73532651+mr-igor25@users.noreply.github.com>
2021-09-04 23:10:20 +0300

    Squashed bugs and added new features!

---
+++ FinTrack/FinTrack/MainViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  FinTrack
+//
+//  Created by Igor Melashchenko on 8/27/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var amountTextField: UITextField!
+    
+    @IBOutlet var currentBalance: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Looks for single or multiple taps.
+         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+
+        view.addGestureRecognizer(tap)
+    }
+
+    //Calls this function when the tap is recognized.
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func newExpenseButton(_ sender: Any) {
+        if let unwrappedPassedAmount = amountTextField.text {
+            let tempAmount: Double = Double(currentBalance.text!)! - Double(unwrappedPassedAmount)!
+            currentBalance.text! = String(tempAmount)
+            
+            amountToReceive = "-" + String(tempAmount)
+            //tabBarController?.selectedIndex = 0 // WHAT IS THIS??? | ask about this line | and DELETE IT
+        }
+    }
+    @IBAction func newIncomeButton(_ sender: Any) {
+        if let unwrappedPassedAmount = amountTextField.text {
+            let tempAmount: Double = Double(currentBalance.text!)! + Double(unwrappedPassedAmount)!
+            currentBalance.text! = String(tempAmount)
+            
+            amountToReceive = "+" + String(tempAmount)
+        }
+    }
+    
+    @IBAction func add10(_ sender: Any) {
+        amountTextField.text = "10"
+    }
+    @IBAction func add50(_ sender: Any) {
+        amountTextField.text = "50"
+    }
+    @IBAction func add100(_ sender: Any) {
+        amountTextField.text = "100"
+    }
+}
+
---
+++ FinTrack/FinTrack/TabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  TabBarController.swift
+//  FinTrack
+//
+//  Created by Igor Melashchenko on 9/4/21.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController { // DELETE THIS FILE
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.selectedIndex = 1
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
